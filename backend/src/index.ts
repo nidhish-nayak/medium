c146985e4b5cf50861203d3a424ac95dce3f2c84
nidhish-nayak <nidhibelthangady@gmail.com>
2024-02-24 20:09:17 +0530

    Removed git cache

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,30 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 
 // Create the main Hono app
-const app = new Hono();
+const app = new Hono<{
+    Bindings: {
+        DATABASE_URL: string;
+    };
+}>();
 
-app.post("/api/v1/signup", (c) => {
-    return c.text("signup route");
+// CRUD Routes
+app.post("/api/v1/signup", async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const body = await c.req.json();
+
+    await prisma.user.create({
+        data: {
+            email: body.email,
+            password: body.password,
+        },
+    });
+
+    return c.text("Hello hoo");
 });
 
 app.post("/api/v1/signin", (c) => {
